686d3134f83ad0e8f8820b00abc33bfb81ab7cfc
stephko1 <stephko@outlook.com>
2021-05-08 12:17:43 +0200

    ios

--- ios/Pipeline/main.m
+++ ios/Pipeline/main.m
@@ -10,6 +10,8 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+    NSArray *array = @[];
+    array[1];
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
