f2c628a35262960a0abe3e0bf07f24ec41dce448
stephko1 <stephko@outlook.com>
2021-05-08 11:55:15 +0200

    ios

--- ios/Pipeline/AppDelegate.m
+++ ios/Pipeline/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+@import UIKit;
+@import Firebase;
 
 @interface AppDelegate ()
 
@@ -14,12 +16,10 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [FIRApp configure];
     return YES;
 }
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
